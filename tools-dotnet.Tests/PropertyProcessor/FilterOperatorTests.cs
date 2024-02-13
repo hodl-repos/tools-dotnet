177f2540db21d9968f5678f0f27de958a5b96b79
Mauracher Simon <simon@mauracher.cc>
2024-02-13 21:04:10 +0100

    added unit test for operator string list starts-with

--- tools-dotnet.Tests/PropertyProcessor/FilterOperatorTests.cs
+++ tools-dotnet.Tests/PropertyProcessor/FilterOperatorTests.cs
@@ -27,5 +27,24 @@ namespace tools_dotnet.Tests.PropertyProcessor
             Assert.That(foundFilter.Apply(v1, v1), Is.True);
             Assert.That(foundFilter.Apply(v1, v2), Is.False);
         }
+
+        [TestCase]
+        public void TestAnyStartsWith()
+        {
+            //Arrange
+            string[] v1 = ["abc", "def"];
+            string[] v2 = ["ab"];
+
+            //Act
+
+            //Assert
+            Assert.That(FilterOperator.Apply("_=", v1 as ICollection<string>, new string[] { "ab" } as ICollection<string>), Is.Not.Null);
+            Assert.That(FilterOperator.Apply("_=", v1 as ICollection<string>, new string[] { "ab" } as ICollection<string>), Is.True);
+            Assert.That(FilterOperator.Apply("!_=", v1 as ICollection<string>, new string[] { "ab" } as ICollection<string>), Is.False);
+
+            Assert.That(FilterOperator.Apply("!_=", v1 as ICollection<string>, new string[] { "rr" } as ICollection<string>), Is.Not.Null);
+            Assert.That(FilterOperator.Apply("!_=", v1 as ICollection<string>, new string[] { "rr" } as ICollection<string>), Is.True);
+            Assert.That(FilterOperator.Apply("_=", v1 as ICollection<string>, new string[] { "rr" } as ICollection<string>), Is.False);
+        }
     }
 }
