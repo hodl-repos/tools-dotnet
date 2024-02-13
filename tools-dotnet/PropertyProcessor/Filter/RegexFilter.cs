3860bad080ee31c039fe3a83ab23ad401bdc25dc
Mauracher Simon <simon@mauracher.cc>
2024-02-13 21:21:17 +0100

    added regex-filter

---
+++ tools-dotnet/PropertyProcessor/Filter/RegexFilter.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace tools_dotnet.PropertyProcessor.Filter
+{
+    public class RegexFilter : IFilter<string, Regex>
+    {
+        public bool Apply(string lhs, Regex rhs)
+        {
+            return rhs.IsMatch(lhs);
+        }
+    }
+}
